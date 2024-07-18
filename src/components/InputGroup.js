3d79b1933a111c195dd567815b1d5190c2927ff4
MysticMaccc <Cosmicsher96@gmail.com>
2024-07-18 13:32:39 +0800

    Implemented initial validation for calorie calculator and finish UI for calorie calculator module

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import ErrorBadge from './ErrorBadge'
 
-function InputGroup({ editMode = false, title, initialValue, className }) {
+function InputGroup({
+    editMode = false,
+    title,
+    initialValue,
+    className,
+    error = null,
+    errorMessage = 'error',
+    ...props
+}) {
+    let inputErrorStyle = error && 'border border-red-500'
+    let titleErrorStyle = error ? ' text-red-500 ' : ' text-gray-700 '
     return (
         <div className={`${className} bg-gray-200 p-2`}>
-            <p className=" text-sm text-gray-700">{title}</p>
-            <p className=" text-lg text-slate-800">{initialValue}</p>
+            <p className={`${titleErrorStyle} text-sm `}>{title}</p>
+            {!editMode && (
+                <p className=" text-lg text-slate-800">{initialValue}</p>
+            )}
+            {editMode && (
+                <input
+                    defaultValue={initialValue}
+                    className={`${inputErrorStyle} w-32 bg-gray-200 text-lg `}
+                    {...props}
+                />
+            )}
+            {error && (
+                <>
+                    <br />
+                    <ErrorBadge message={errorMessage} />
+                </>
+            )}
         </div>
     )
 }
