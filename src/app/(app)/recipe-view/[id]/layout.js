60508a45e271d33d78dd630e9a1efcda04358d4a
NOCDEPT <angeloperia3@gmail.com>
2024-08-01 16:37:13 +0800

    add a tabs in recipe view

--- src/app/(app)/recipe-view/[id]/layout.js
+++ src/app/(app)/recipe-view/[id]/layout.js
@@ -4,7 +4,7 @@ function layout({ children }) {
     return (
         <div className='flex flex-col min-h-screen overflow-x-hidden'>
             <div className='flex-1 bg-slate-50 p-4 achor'>
-                <div className='mx-auto max-w-screen-xl'>{children}</div>
+                <div className='mx-auto max-w-screen-xl mb-10'>{children}</div>
             </div>
         </div>
     );
