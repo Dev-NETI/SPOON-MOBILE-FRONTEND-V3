d2961cf523aa93e8a39dd05d8018314718272182
MysticMaccc <cosmicsher96@gmail.com>
2024-08-06 14:20:21 +0800

    Continuous development of nutrient calculator

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -95,6 +95,15 @@ const computeBloodPressure = (sys, dia) => {
     return 'Unknown';
 };
 
+const getCurrentDate = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+};
+
 export {
     cn,
     computeAge,
@@ -105,4 +114,5 @@ export {
     passwordRules,
     calculateBMI,
     computeBloodPressure,
+    getCurrentDate,
 };
