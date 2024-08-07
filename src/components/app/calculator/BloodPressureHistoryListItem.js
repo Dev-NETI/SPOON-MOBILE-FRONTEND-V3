99e59b6d70c3ec14346f1fa4e8fb2e168093a421
MysticMaccc <cosmicsher96@gmail.com>
2024-08-07 14:57:40 +0800

    fix console errors in blood pressure module

--- src/components/app/calculator/BloodPressureHistoryListItem.js
+++ src/components/app/calculator/BloodPressureHistoryListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import CardListItemComponent from './CardListItemComponent';
+import { formatDate } from '@/lib/utils';
 
 function BloodPressureHistoryListItem({ data }) {
     return (
         <CardListItemComponent
             title={data.bpCategory}
             name={`${data.systolic} / ${data.diastolic}`}
-            created_at={data.created_at}
+            created_at={formatDate(data.created_at, 'dd-mm-yyyy hh:mm:ss')}
         />
     );
 }
