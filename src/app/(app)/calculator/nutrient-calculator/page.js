94cf59608b5c184d61530db04fcc361fb405fa84
MysticMaccc <cosmicsher96@gmail.com>
2024-08-07 11:35:53 +0800

    added date picker to nutrient calculator

--- src/app/(app)/calculator/nutrient-calculator/page.js
+++ src/app/(app)/calculator/nutrient-calculator/page.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import NutrientCalculatorResultComponent from '@/components/app/calculator/NutrientCalculatorResultComponent';
 import LogFoodContainer from '@/components/app/calculator/LogFoodContainer';
+import { DatePicker } from '@/components/ui/date-picker';
+import { getCurrentDate } from '@/lib/utils';
 
 function page() {
     const [foodData, setFoodData] = useState({
@@ -12,14 +14,24 @@ function page() {
         totalSodium: 0,
         totalFiber: 0,
     });
+    const [date, setDate] = useState(getCurrentDate());
+
     return (
-        <div
-            className='basis-full  gap-2 p-10 md:p-5 lg:p-5  
+        <>
+            <div className='basis-full flex justify-center items-center p-4 -mb-8'>
+                <DatePicker date={date} setDate={setDate} />
+            </div>
+            <div
+                className='basis-full  gap-2 p-10 md:p-5 lg:p-5  
                        flex flex-col md:flex-row lg:flex-row '
-        >
-            <NutrientCalculatorResultComponent data={foodData} />
-            <LogFoodContainer setParentState={setFoodData} />
-        </div>
+            >
+                <NutrientCalculatorResultComponent data={foodData} />
+                <LogFoodContainer
+                    setParentState={setFoodData}
+                    currentDate={date}
+                />
+            </div>
+        </>
     );
 }
 
--- src/components/ProgressBarWLabel.js
+++ src/components/ProgressBarWLabel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { calculatePercentage } from '@/lib/utils';
 
 function ProgressBarWLabel({
@@ -10,18 +11,21 @@ function ProgressBarWLabel({
 }) {
     const width = calculatePercentage(value, appropriateValue);
     const progressText = `${value.toFixed(2)} g / ${appropriateValue} g`;
+
     return (
         <>
-            <div className={`${labelClassName} mb-1 text-base font-medium`}>
-                {label}
+            <div className={`${labelClassName} mb-1 text-xs font-semibold`}>
+                {label} - {progressText}
             </div>
-            <div className='w-full bg-gray-200 rounded-full h-4 mb-4 '>
-                <div
-                    className={`${progressClassName} h-4 rounded-full text-center text-xs items-center text-slate-100`}
-                    style={{ width: `${width}%` }}
+            <div className='w-full bg-gray-200 rounded-full h-4 mb-4'>
+                <motion.div
+                    className={`${progressClassName} h-4 rounded-full text-center text-xs items-center text-stone-900`}
+                    initial={{ width: 0 }}
+                    animate={{ width: `${width}%` }}
+                    transition={{ duration: 0.5 }}
                 >
-                    {width !== 0 && progressText}
-                </div>
+                    {width}%
+                </motion.div>
             </div>
         </>
     );
