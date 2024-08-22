94b96a48365b93c9f2faddc443177041ee54c4a5
MysticMaccc <cosmicsher96@gmail.com>
2024-08-22 09:49:22 +0800

    put spoon login page as default home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,24 @@
-import { Nunito } from 'next/font/google'
-import '@/app/global.css'
+import { Nunito } from 'next/font/google';
+import '@/app/global.css';
+import LoginComponent from '@/components/auth/LoginComponent';
 
 const nunitoFont = Nunito({
     subsets: ['latin'],
     display: 'swap',
-})
+});
 
 const RootLayout = ({ children }) => {
     return (
-        <html lang="en" className={nunitoFont.className}>
-            <body className="antialiased">{children}</body>
+        <html lang='en' className={nunitoFont.className}>
+            <body className='antialiased'>
+                <LoginComponent>{children}</LoginComponent>
+            </body>
         </html>
-    )
-}
+    );
+};
 
 export const metadata = {
     title: 'SPOON.PH',
-}
+};
 
-export default RootLayout
+export default RootLayout;
