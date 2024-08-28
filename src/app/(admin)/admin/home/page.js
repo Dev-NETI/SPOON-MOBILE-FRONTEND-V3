6e5ae3ff2896b6cfbff466ae6bbeb0a60886a2b2
NOCDEPT <angeloperia3@gmail.com>
2024-08-28 09:58:30 +0800

    add a top nav. in admin layout

---
+++ src/app/(admin)/admin/home/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function page() {
+    return (
+        <>
+            <div className='ocean'>
+                <div className='wave'></div>
+                <div className='wave'></div>
+                <div className='wave'></div>
+            </div>
+        </>
+    );
+}
+
+export default page;
--- src/components/ApplicationLogo.js
+++ src/components/ApplicationLogo.js
@@ -1,7 +1,7 @@
-import logo from '../../public/images/spoon_logo.png'
-import Image from 'next/image'
+import logo from '../../public/images/spoon_logo.png';
+import Image from 'next/image';
 const ApplicationLogo = () => (
-    <Image src={logo} alt="Spoon Logo" width={200} height="auto" priority />
-)
+    <Image src={logo} alt='Spoon Logo' width={200} height='auto' priority />
+);
 
-export default ApplicationLogo
+export default ApplicationLogo;
